777effa446728e7ff52c02315c29a37188d2249f
Vaishnavi sharma <101987092+vaishnavi10S@users.noreply.github.com>
2025-12-17 20:41:23 +0530

    Create leap2.js

---
+++ leap2.js
@@ -0,0 +1,24 @@
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+    
+    //Write your code here.    
+    if (year %4===0){
+          if (year %100===0){
+                if (year %400===0){
+                    return "leap year";  
+                } else{
+                     return " not leap year"; 
+                } else {
+                return "leap year"
+                }
+                      
+          }
+    } 
+      
+    }
+      
+    
+
+/**************Don't change the code below****************/    
+
