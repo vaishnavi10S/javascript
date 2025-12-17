0e2f03a30036c757878ca5aa930bc7092766284f
Vaishnavi sharma <101987092+vaishnavi10S@users.noreply.github.com>
2025-12-17 20:39:36 +0530

    Create random.js

---
+++ random.js
@@ -0,0 +1,16 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+   
+   var noOfPeople=names.length;
+   var RandomPos=Math.floor(Math.random()* noOfPeople);
+   var RandomP= names[RandomPos];
+   return RandomP +" is going to buy lunch today!";
+    
+    
+
+
+/******Don't change the code below*******/    
+}
